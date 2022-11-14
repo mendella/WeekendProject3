969f285cc67f266686ac6cbdf46072be7fabd674
Bruno Mendella <66249806+mendella@users.noreply.github.com>
2022-11-14 15:56:03 -0300

    Update ERC20Votes.sol

--- contracts/ERC20Votes.sol
+++ contracts/ERC20Votes.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
